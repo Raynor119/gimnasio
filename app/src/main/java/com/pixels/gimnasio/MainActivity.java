ca49f000f07a4514a57ddd90cda9089c231290e1
Raynor9029 <saibod15@hotmail.es>
2020-11-17 10:31:45 -0500

    alpha 0.1.1

--- app/src/main/java/com/pixels/gimnasio/MainActivity.java
+++ app/src/main/java/com/pixels/gimnasio/MainActivity.java
@@ -1,15 +1,25 @@
 package com.pixels.gimnasio;
 
 import android.app.*;
+import android.content.Intent;
 import android.os.*;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText email,conta;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email=(EditText) findViewById(R.id.editTextEmail);
+        conta=(EditText) findViewById(R.id.editTextPassword);
+    }
+    public void onclic(View view){
+        Intent intent =new Intent(MainActivity.this,cargar.class);
+        startActivity(intent);
     }
 }
